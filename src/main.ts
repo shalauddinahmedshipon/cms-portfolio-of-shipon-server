bf239007cc6acb64abeb55f6c3726685db99a64c
MD. SHIPON <sshimi910@gmail.com>
2026-02-02 16:59:34 +0600

    update cors origin

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: ['http://localhost:3000','https://cms-portfolio-of-shipon-client.vercel.app'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
